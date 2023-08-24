67a145249b7be6b33da2c1ff286f4bdb8edeecfe
tstrassb <tstrassb@student.42wolfsburg.de>
2023-08-24 10:06:24 +0200

    fixed update_param

--- srcs/ft_raycaster.c
+++ srcs/ft_raycaster.c
@@ -6,7 +6,7 @@
 /*   By: tstrassb <tstrassb@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 08:54:55 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/08/23 10:32:15 by tstrassb         ###   ########.fr       */
+/*   Updated: 2023/08/24 09:24:19 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,11 +96,9 @@ void	check_side(t_data *data)
 void	dda_calcu(t_data *data)
 {
 	data->dda->cam_x = 2 * (double)data->dda->screen_x / (double)WINDOW_W - 1;
-	data->dda->ray_dir_x = data->player->dir_x + \
-		data->player->player_x * data->dda->cam_x;
-	data->dda->ray_dir_y = data->player->dir_y + \
-		data->player->player_y * data->dda->cam_x;
-	data->dda->map_x = (int)data->player->pos_x;
+	data->dda->ray_dir_x = data->player->dir_x + data->player->player_x * data->dda->cam_x;
+	data->dda->ray_dir_y = data->player->dir_y + data->player->player_y * data->dda->cam_x;
+	data->dda->map_x = (int)data->player->pos_x ;
 	data->dda->map_y = (int)data->player->pos_y;
 	data->dda->delta_dist_x = fabs(1 / data->dda->ray_dir_x);
 	data->dda->delta_dist_y = fabs(1 / data->dda->ray_dir_y);
