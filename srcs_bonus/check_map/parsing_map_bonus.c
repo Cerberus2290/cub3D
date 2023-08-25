ecfab8d3d3c5077b46723c82a48b910680badc0d
tstrassb <tstrassb@student.42wolfsburg.de>
2023-08-25 16:59:42 +0200

    redesigned sprite textures

--- srcs_bonus/check_map/parsing_map_bonus.c
+++ srcs_bonus/check_map/parsing_map_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tstrassb <tstrassb@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 10:02:05 by tstrassb          #+#    #+#             */
-/*   Updated: 2023/08/25 16:18:11 by tstrassb         ###   ########.fr       */
+/*   Updated: 2023/08/25 16:57:16 by tstrassb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,13 +82,13 @@ int	file_convert(t_data *data, t_map *map, t_files *files)
 	init_xpm(data, 4, files->east);
 	ft_free(files->north, files->south, files->west, files->east);
 	//init_xpm(data, 5, "./resources/door_00.xpm");
-	init_xpm(data, 6, "./resources/object_01.xpm");
+	init_xpm(data, 6, "./resources/object_01-1.xpm");
 	init_xpm(data, 7, "./resources/object_02.xpm");
-	init_xpm(data, 8, "./resources/object_03.xpm");
+	init_xpm(data, 8, "./resources/object_03-1.xpm");
 	init_xpm(data, 9, "./resources/object_04.xpm");
-	init_xpm(data, 10, "./resources/object_05.xpm");
+	init_xpm(data, 10, "./resources/object_05-1.xpm");
 	init_xpm(data, 11, "./resources/object_06.xpm");
-	init_xpm(data, 12, "./resources/object_01.xpm");
+	init_xpm(data, 12, "./resources/object_01-1.xpm");
 	//init_xpm(data, 13, "./resources/door_01.xpm");
 	//init_xpm(data, 14, "./resources/door_02.xpm");
 	//init_xpm(data, 15, "./resources/door_03.xpm");
